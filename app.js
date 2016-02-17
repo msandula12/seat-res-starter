93790568078d9507a2dc9bb69d820b4af3a9213a
Tom <tomalexanderphoto@gmail.com>
2016-02-17 14:25:13 -0500

    toggle class on click in JS and working on the form

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("Hello, World!");
+$(document).ready(function(){
+
+		$("div.available").click(function(){
+		$(this).toggleClass("reserved");
+
+	});
+
+$(Form).submit(function() {
+    // get all the inputs into an array.
+    var $values = $(this).serialize();
+
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    // var values = {};
+    // $inputs.each(function() {
+    //     values[this.name] = $(this).val();
+    // });
+
+});
+
+// document.forms[form].getElementsbyId('custName');
+		// $( "form" ).data( "test", {first: "Tom", last: "Alexander"});
+		// $( "span:first" ).text( $( "div.reserved" ).data("test").first )
+});
+
+
+	// $('submitButt').click(function){
+	// 	getElementById('custName').value;
+
+
+// function getInputElements() {
+// 	var custName = document.getElementsbyId('custName');
+
+
+
+	// var custName = document.getElementById('custName').value;
+	// var custEmail = document.getElementById('custEmail').value;
+	// 	$('submitButt').click(function(){
+	// 	$('#first').append("<span></span>");
+	// 	}
