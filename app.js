2fb08e0cd478d2a7633a5f8f90b5d877193a9832
Tom <tomalexanderphoto@gmail.com>
2016-02-17 15:43:22 -0500

    generated <div> field with a for loop in the js file

--- app.js
+++ app.js
@@ -1,46 +1,24 @@
 $(document).ready(function(){
+	
+			var button = $('#submitButt')
+			var seatNumber = [];
+			//NEW FUNK TION HERE
+			for(var i=1; i<=24; i++){
+				seatNumber.push(i);
+				$("#seat-chart").append(function(){
+					return "<div class='available'>" + i + "</div>";
+				})
+			}
 
 		$("div.available").click(function(){
 		$(this).toggleClass("reserved");
 
+		});
+
+
 	});
 
-$(Form).submit(function() {
-    // get all the inputs into an array.
-    var $values = $(this).serialize();
-
-    // not sure if you wanted this, but I thought I'd add it.
-    // get an associative array of just the values.
-    // var values = {};
-    // $inputs.each(function() {
-    //     values[this.name] = $(this).val();
-    // });
 
 
 
-
-
-
-
-});
-
-// document.forms[form].getElementsbyId('custName');
-		// $( "form" ).data( "test", {first: "Tom", last: "Alexander"});
-		// $( "span:first" ).text( $( "div.reserved" ).data("test").first )
-});
-
-
-	// $('submitButt').click(function){
-	// 	getElementById('custName').value;
-
-
-// function getInputElements() {
-// 	var custName = document.getElementsbyId('custName');
-
-
-
-	// var custName = document.getElementById('custName').value;
-	// var custEmail = document.getElementById('custEmail').value;
-	// 	$('submitButt').click(function(){
-	// 	$('#first').append("<span></span>");
-	// 	}
+	
