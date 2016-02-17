97366b4e887435b3820ab02505f91987b96e9be6
Tom <tomalexanderphoto@gmail.com>
2016-02-17 16:59:33 -0500

    added seat numbers to form, took out divs from html and generated them with a for loop

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	
+
 			var button = $('#submitButt')
 			var seatNumber = [];
 			//NEW FUNK TION HERE
 			for(var i=1; i<=24; i++){
 				seatNumber.push(i);
 				$("#seat-chart").append(function(){
-					return "<div class='available'>" + i + "</div>";
+					return "<div class='available'><h3>" + i + "</h3></div>";
 				})
 			}
 
@@ -16,7 +16,18 @@ $(document).ready(function(){
 		});
 
 
+		$('.available').on('click', function() {
+			$('#seat').replaceWith('test');
+		
+
 	});
+		// function showValues() {
+		// 	var results = $(":input").serializeArray();
+		// 	// $('#seat-chart').empty();
+		// 	jQuery.each( results, function( i, field)
+		// 		$( "#seat-chart" ).append(field.value + " ");
+		// });
+});
 
 
 
