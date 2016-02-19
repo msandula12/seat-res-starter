b84a243f228fd063c1e41873b0284d11d8d6be6b
Tom <tomalexanderphoto@gmail.com>
2016-02-19 14:45:52 -0500

    almost done merging

--- branchlab.js
+++ branchlab.js
@@ -2,11 +2,7 @@ console.log("My name is LGOD_Tom here's a new file");
 //aleah made a change on line 3, it was super fly
 document.write("Branch test --Mike");
 console.log("My name is Mike here's a new file");
-
-
 prompt('Are you Kwaku?);
-
-
 //aleah made a change on line 3, it was super fly
 document.write("Branch test --Mike");
 
