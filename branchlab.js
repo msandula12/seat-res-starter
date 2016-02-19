2282dc2966deb3f5087809ed86a97fe94d3e6e20
Tom <tomalexanderphoto@gmail.com>
2016-02-19 14:47:03 -0500

    JOHN CENA

--- branchlab.js
+++ branchlab.js
@@ -1,4 +1,4 @@
-console.log("My name is LGOD_Tom here's a new file");
+console.log("My name is JOHN CENA here's a new file");
 //aleah made a change on line 3, it was super fly
 document.write("Branch test --Mike");
 console.log("My name is Mike here's a new file");
